9aae65eb908ee0c75b4893dd5da56e3f08606148
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2025-06-28 00:07:01 +0200

    reverts auto-start of clojure sessions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -688,13 +688,7 @@
   :defer t
   :custom
   (cider-repl-display-help-banner nil)
-  (nrepl-sync-request-timeout 600)
-  :hook ((clojure-mode . rb/start-sesman-session))
-  :config
-  (defun rb/start-sesman-session ()
-    "Ensure an active Sesman session when working in Clojure."
-    (unless (sesman-current-session 'CIDER)
-      (cider-jack-in-clj nil))))
+  (nrepl-sync-request-timeout 600))
 
 (use-package clj-refactor
   :ensure t
